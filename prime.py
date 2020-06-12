8aec34bd89ce826108d9506b3b6ade3799c89dfa
Shaikh Numaan <53473560+sknum@users.noreply.github.com>
2020-06-12 17:03:59 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,9 +1,10 @@
-lower = int(input("enter the lower interval:"))
-upper = int(input("enter the upper interval:")) 
-for val in range(lower, upper + 1): 
- if val > 1:
-  for n in range(2, val): 
-   if (val % n) == 0 :  
-    break
-  else:   
-    print(val) 
+start = 11
+end = 25
+for val in range(start, end + 1): 
+	if val > 1: 
+		for n in range(2, val//2 + 2): 
+			if (val % n) == 0: 
+				break
+			else: 
+				if n == val//2 + 1: 
+					print(val) 
